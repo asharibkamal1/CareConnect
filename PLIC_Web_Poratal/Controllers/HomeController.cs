871e3db1e472864ab29763bf922145fb717caa7b
Asharib Kamal <asharibkamal1@gmail.com>
2023-09-12 15:45:24 +0500

    add if condition

--- PLIC_Web_Poratal/Controllers/HomeController.cs
+++ PLIC_Web_Poratal/Controllers/HomeController.cs
@@ -1582,6 +1582,12 @@ namespace PLIC_Web_Poratal.Controllers
                         // Check if the ticket already exists in your database
                         //bool ticketExists = (bool)cmd.Parameters["@ticketExists"].Value;
 
+                        if (true)
+                        {
+
+                        }
+
+
                         bool ticketExists = (bool)cmd.Parameters["@ticketExists"].Value;
                         int ticketID = (int)cmd.Parameters["@TicketID"].Value;
                         string Complainer = (string)cmd.Parameters["@Complainer"].Value;
